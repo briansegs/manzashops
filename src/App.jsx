cf992e0034f00ee5de3c6022a464a6d3b882dfab
bsegers <briansegs@gmail.com>
2024-11-11 19:52:51 -0500

    setup: React, Tailwindcss, folders

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="flex w-full justify-center">
+      <h1 className="text-3xl font-bold underline">Manzashop.com</h1>
+    </div>
+  );
+};
+
+export default App;
