b88c04a7825f8a425afca3e4bd75c30562f83c2c
bsegers <briansegs@gmail.com>
2024-11-12 15:35:56 -0500

    add: App

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import ImageSlider from "./components/ImageSlider";
+import Languages from "./components/Languages";
+import Header from "./containers/Header";
+import Menu from "./containers/Menu";
+import Suggestions from "./containers/Suggestions";
+
 const App = () => {
   return (
-    <div className="flex w-full justify-center">
-      <h1 className="text-3xl font-bold underline">Manzashop.com</h1>
+    <div className="">
+      <Header />
+      <Menu />
+      <ImageSlider />
+      <Languages />
+      <Suggestions />
     </div>
   );
 };
