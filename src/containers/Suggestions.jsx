cea506410ac09646453054227d889782b8885b15
bsegers <briansegs@gmail.com>
2024-11-12 15:35:21 -0500

    add: Suggestions

---
+++ src/containers/Suggestions.jsx
@@ -0,0 +1,104 @@
+const Suggestions = () => {
+  return (
+    <div className="sticky top-0 flex items-center justify-between bg-[#141414] border-[3px] border-black h-[17vh] text-white overflow-x-scroll mt-[-5vh]">
+      {/* add hover to img -> hover{box-shadow: 3px 3px 3px #60b3d1;} */}
+      <a
+        href=""
+        className="mt-[1vh] text-[28px] text-white border-[1px] border-[#1b1e23] bg-black overflow-x-scroll rounded-[50%] w-[100px] h-[100px] justify-center items-center hover:text-[#60b3d1]"
+        style={{ boxShadow: "3px 3px 3px white" }}
+      >
+        Top
+      </a>
+      {/* add hover to img -> hover{box-shadow: 3px 3px 3px #60b3d1;} */}
+      <a
+        href="#hit"
+        className="mt-[1vh] text-[28px] text-white border-[1px] border-[#1b1e23] bg-black overflow-x-scroll rounded-[50%] w-[100px] h-[100px] justify-center items-center hover:text-[#60b3d1]"
+        style={{ boxShadow: "3px 3px 3px white" }}
+      >
+        hit
+      </a>
+      {/* add hover to img -> hover{box-shadow: 3px 3px 3px #60b3d1;} */}
+      <a
+        href="#hit"
+        className="mt-[1vh] text-[28px] text-white border-[1px] border-[#1b1e23] bg-black overflow-x-scroll rounded-[50%] w-[100px] h-[100px] justify-center items-center hover:text-[#60b3d1]"
+        style={{ boxShadow: "3px 3px 3px white" }}
+      >
+        piano
+      </a>
+      {/* add hover to img -> hover{box-shadow: 3px 3px 3px #60b3d1;} */}
+      <a
+        href="#hit"
+        className="mt-[1vh] text-[28px] text-white border-[1px] border-[#1b1e23] bg-black overflow-x-scroll rounded-[50%] w-[100px] h-[100px] justify-center items-center hover:text-[#60b3d1]"
+        style={{ boxShadow: "3px 3px 3px white" }}
+      >
+        surf
+      </a>
+      {/* add hover to img -> hover{box-shadow: 3px 3px 3px #60b3d1;} */}
+      <a
+        href="#hit"
+        className="mt-[1vh] text-[28px] text-white border-[1px] border-[#1b1e23] bg-black overflow-x-scroll rounded-[50%] w-[100px] h-[100px] justify-center items-center hover:text-[#60b3d1]"
+        style={{ boxShadow: "3px 3px 3px white" }}
+      >
+        Tech
+      </a>
+      {/* add hover to img -> hover{box-shadow: 3px 3px 3px #60b3d1;} */}
+      <a
+        href="#hit"
+        className="mt-[1vh] text-[28px] text-white border-[1px] border-[#1b1e23] bg-black overflow-x-scroll rounded-[50%] w-[100px] h-[100px] justify-center items-center hover:text-[#60b3d1]"
+        style={{ boxShadow: "3px 3px 3px white" }}
+      >
+        dress
+      </a>
+      {/* add hover to img -> hover{box-shadow: 3px 3px 3px #60b3d1;} */}
+      <a
+        href="#hit"
+        className="mt-[1vh] text-[28px] text-white border-[1px] border-[#1b1e23] bg-black overflow-x-scroll rounded-[50%] w-[100px] h-[100px] justify-center items-center hover:text-[#60b3d1]"
+        style={{ boxShadow: "3px 3px 3px white" }}
+      >
+        glasses
+      </a>
+      {/* add hover to img -> hover{box-shadow: 3px 3px 3px #60b3d1;} */}
+      <a
+        href="#hit"
+        className="mt-[1vh] text-[28px] text-white border-[1px] border-[#1b1e23] bg-black overflow-x-scroll rounded-[50%] w-[100px] h-[100px] justify-center items-center hover:text-[#60b3d1]"
+        style={{ boxShadow: "3px 3px 3px white" }}
+      >
+        shoes
+      </a>
+      {/* add hover to img -> hover{box-shadow: 3px 3px 3px #60b3d1;} */}
+      <a
+        href="#hit"
+        className="mt-[1vh] text-[28px] text-white border-[1px] border-[#1b1e23] bg-black overflow-x-scroll rounded-[50%] w-[100px] h-[100px] justify-center items-center hover:text-[#60b3d1]"
+        style={{ boxShadow: "3px 3px 3px white" }}
+      >
+        jewelery
+      </a>
+      {/* add hover to img -> hover{box-shadow: 3px 3px 3px #60b3d1;} */}
+      <a
+        href="#hit"
+        className="mt-[1vh] text-[28px] text-white border-[1px] border-[#1b1e23] bg-black overflow-x-scroll rounded-[50%] w-[100px] h-[100px] justify-center items-center hover:text-[#60b3d1]"
+        style={{ boxShadow: "3px 3px 3px white" }}
+      >
+        carparts
+      </a>
+      {/* add hover to img -> hover{box-shadow: 3px 3px 3px #60b3d1;} */}
+      <a
+        href="#hit"
+        className="mt-[1vh] text-[28px] text-white border-[1px] border-[#1b1e23] bg-black overflow-x-scroll rounded-[50%] w-[100px] h-[100px] justify-center items-center hover:text-[#60b3d1]"
+        style={{ boxShadow: "3px 3px 3px white" }}
+      >
+        sports
+      </a>
+      {/* add hover to img -> hover{box-shadow: 3px 3px 3px #60b3d1;} */}
+      <a
+        href="#hit"
+        className="mt-[1vh] text-[28px] text-white border-[1px] border-[#1b1e23] bg-black overflow-x-scroll rounded-[50%] w-[100px] h-[100px] justify-center items-center hover:text-[#60b3d1]"
+        style={{ boxShadow: "3px 3px 3px white" }}
+      >
+        luxury
+      </a>
+    </div>
+  );
+};
+
+export default Suggestions;
