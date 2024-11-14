3db42120afa3b1416446243d72d200b93effdd1c
bsegers <briansegs@gmail.com>
2024-11-13 19:34:42 -0500

    add: make menu responsive/ redesign code / make burger menu

--- src/containers/Header.jsx
+++ src/containers/Header.jsx
@@ -1,4 +1,10 @@
+import { useState } from "react";
+import { RxHamburgerMenu, RxCross1 } from "react-icons/rx";
+import NavLinks from "../components/nav/NavLinks";
+import { navItems } from "../constants";
+
 const Header = () => {
+  const [toggle, setToggle] = useState(false);
   return (
     <div className="flex justify-between items-center bg-header h-[50px] px-16 xl:px-40">
       <a href="/apl/shop1.html" className="text-white hover:text-secondary">
@@ -10,7 +16,7 @@ const Header = () => {
         placeholder="Search..."
       />
 
-      <div className="flex lg:gap-16 md:gap-4">
+      <div className="lg:flex lg:gap-16 md:gap-4 hidden">
         <a href="#" className="text-white px-2">
           Our Mission
         </a>
@@ -18,6 +24,43 @@ const Header = () => {
         <a href="checkout.html" className="text-white px-2">
           Support Us
         </a>
+      </div>
+
+      <div className="lg:hidden flex flex-1 justify-end items-center">
+        <button type="button" onClick={() => setToggle((prev) => !prev)}>
+          {toggle ? (
+            <RxCross1 className="text-white size-7" />
+          ) : (
+            <RxHamburgerMenu className="text-white size-7" />
+          )}
+        </button>
+
+        <div
+          className={`${
+            toggle ? "flex" : "hidden"
+          } pb-6 bg-navbar flex-col absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-lg sidebar border-t-4 border-secondary`}
+        >
+          <div className="px-3 p-6  bg-black">
+            <input
+              className="border-black border-[3px] rounded-[5px] w-full h-8 "
+              placeholder="Search..."
+            />
+          </div>
+
+          <div className="w-full flex gap-4 justify-between pb-3 px-6 bg-black mb-3">
+            <a href="#" className="text-white px-2">
+              Our Mission
+            </a>
+
+            <a href="checkout.html" className="text-white px-2">
+              Support Us
+            </a>
+          </div>
+
+          <ul className="flex flex-col flex-1 px-6 gap-2">
+            <NavLinks navItems={navItems} />
+          </ul>
+        </div>
       </div>
     </div>
   );
--- src/containers/Menu.jsx
+++ src/containers/Menu.jsx
@@ -1,67 +1,13 @@
 import NavLinks from "../components/nav/NavLinks";
-
-const navItems = [
-  {
-    href: "/Scope1.html",
-    name: "Home",
-    line: false,
-    black: false,
-  },
-  {
-    href: "/shop1.html",
-    name: "Dash",
-    line: true,
-    black: true,
-  },
-  {
-    href: "/deals.html",
-    name: "Deals",
-    line: true,
-    black: false,
-  },
-  {
-    href: "/",
-    name: "Services",
-    line: true,
-    black: false,
-  },
-  {
-    href: "/",
-    name: "Jobs",
-    line: true,
-    black: false,
-  },
-  {
-    href: "/",
-    name: "Menus",
-    line: true,
-    black: false,
-  },
-  {
-    href: "/movies.html",
-    name: "Movies",
-    line: true,
-    black: false,
-  },
-  {
-    href: "/articles1.html",
-    name: "Newsletter",
-    line: true,
-    black: false,
-  },
-  {
-    href: "/",
-    name: "Great Things to try",
-    line: true,
-    black: false,
-  },
-];
+import { navItems } from "../constants";
 
 const Menu = () => {
   return (
-    <ul className="rounded-b-[10px] flex items-center justify-around bg-navbar border-[3px] border-black h-[50px]">
-      <NavLinks navItems={navItems} />
-    </ul>
+    <nav className="rounded-b-[10px] bg-navbar border-[3px] border-black h-3 lg:h-[50px]">
+      <ul className="hidden lg:flex h-full justify-around items-center">
+        <NavLinks navItems={navItems} />
+      </ul>
+    </nav>
   );
 };
 
