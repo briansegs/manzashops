65e2fbb2dd4e9078b7d70a6b57457123a17dead6
bsegers <briansegs@gmail.com>
2024-11-14 17:15:09 -0500

    add: make footer respnsive / redesign code

--- src/containers/Footer.jsx
+++ src/containers/Footer.jsx
@@ -1,82 +1,66 @@
 const Footer = () => {
   return (
-    <footer className="pt-[5vh] text-[#f2f2f2] bg-black w-[100vw] h-[50vh] hover:bg-black">
-      <div className="w-[20vw] h-[20vh]">
-        <img
-          src="/src/assets/favicon.png"
-          className="ml-5vh w-[20vw] h-[30vh] border-[1px] border-white"
-        />
-      </div>
-      <div className="mt-[10vh] ml-[20vh] w-[10vw] h-[10vh] bg-black">
-        <p className="text-[28px]">Est.2024</p>
+    <footer className="flex flex-col pt-8 pb-12 px-9 w-full text-[#f2f2f2] bg-black">
+      <div className="flex lg:flex-row flex-col w-full">
+        <div className="flex flex-col items-center mr-8">
+          <img
+            src="/src/assets/favicon.png"
+            className=" w-96 border-[1px] border-white"
+          />
+
+          <p className="text-[28px]">Est.2024</p>
+        </div>
+
+        <div className="flex lg:w-7/12 w-full justify-around">
+          <div className="">
+            <p className="text-[28px]">more</p>
+            {Array(5)
+              .fill()
+              .map((_, index) => (
+                <ul key={index} className="pl-12">
+                  <a href="" className="inline-block text-blue-white mt-[2vh]">
+                    contact
+                  </a>{" "}
+                </ul>
+              ))}
+          </div>
+
+          <div className="">
+            <p className="text-[28px]">more</p>
+            {Array(5)
+              .fill()
+              .map((_, index) => (
+                <ul key={index} className="pl-12">
+                  <a href="" className="inline-block text-blue-white mt-[2vh]">
+                    contact
+                  </a>{" "}
+                </ul>
+              ))}
+          </div>
+
+          <div className="">
+            <p className="text-[28px]">more</p>
+            {Array(5)
+              .fill()
+              .map((_, index) => (
+                <ul key={index} className="pl-12">
+                  <a href="" className="inline-block text-blue-white mt-[2vh]">
+                    contact
+                  </a>{" "}
+                </ul>
+              ))}
+          </div>
+        </div>
       </div>
 
-      <div className="w-[15vw] h-[30vh] bg-black mt-[-40vh] ml-[60vh] block">
-        <p className="text-[28px]">more</p>
-        {Array(5)
-          .fill()
-          .map((_, index) => (
-            <ul key={index}>
-              <a href="" className="inline-block text-blue-white mt-[2vh]">
-                contact
-              </a>{" "}
-            </ul>
-          ))}
-      </div>
+      <div className="flex w-full md:flex-row flex-col mt-4">
+        <p className="text-[28px] w-1/2 flex md:justify-end justify-start">
+          Socials
+        </p>
 
-      <div className="w-[15vw] h-[30vh] bg-black mt-[-30vh] ml-[100vh] block">
-        <p className="text-[28px]">more</p>
-        {Array(5)
-          .fill()
-          .map((_, index) => (
-            <ul key={index}>
-              <a href="" className="inline-block text-blue-white mt-[2vh]">
-                contact
-              </a>{" "}
-            </ul>
-          ))}
-      </div>
-
-      <div className="w-[15vw] h-[30vh] bg-black mt-[-30vh] ml-[140vh] block">
-        <p className="text-[28px]">more</p>
-        {Array(5)
-          .fill()
-          .map((_, index) => (
-            <ul key={index}>
-              <a href="" className="inline-block text-blue-white mt-[2vh]">
-                contact
-              </a>{" "}
-            </ul>
-          ))}
-      </div>
-
-      <div className="w-[40vw] h-[15vh] bg-black mt-0 ml-[90vh] flex">
-        <p className="text-[28px]">Socials</p>
-        <ul>
-          {" "}
-          <a href="" className="flex text-white mt-[6vh]">
-            contact
-          </a>{" "}
-        </ul>
-        <ul>
-          <a href="" className="flex text-white mt-[6vh]">
-            contact
-          </a>
-        </ul>
-        <ul>
-          <a href="" className="flex text-white mt-[6vh]">
-            contact
-          </a>{" "}
-        </ul>
-        <ul>
-          <a href="" className="flex text-white mt-[6vh]">
-            contact
-          </a>{" "}
-        </ul>
-        <ul>
-          <a href="" className="flex text-white mt-[6vh]">
-            contact
-          </a>{" "}
+        <ul className="flex md:pl-8 pl-2 md:gap-10 gap-2 lg:w-1/2 w-full justify-around md:justify-start md:mt-12 mt-2">
+          <a href="">contact</a> <a href="">contact</a>
+          <a href="">contact</a> <a href="">contact</a> <a href="">contact</a>{" "}
         </ul>
       </div>
     </footer>
