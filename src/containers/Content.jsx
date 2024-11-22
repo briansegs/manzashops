64c8dad78d697a03f7960ef82421d76fdef5e414
bsegers <briansegs@gmail.com>
2024-11-21 19:23:43 -0500

    fix: Missing href

--- src/containers/Content.jsx
+++ src/containers/Content.jsx
@@ -8,19 +8,19 @@ const data = [
     src: "/manzashops/assets/ad/webflow.jpeg",
   },
   {
-    href: "https://www.aldoshoes.com/us/en_US?gclsrc=aw.ds&&utm_source=google&utm_medium=cpc&utm_campaign=aldo_us_en_search_brand_nogender_core&utm_content=653404197253&utm_term=aldo&device=c&network=g&placement=&gad_source=1&gclid=EAIaIQobChMI1_jp_KaxiQMVrm9HAR0_Sj1YEAAYASAAEgKjY_D_BwE",
+    href: mockHref,
     src: "/manzashops/assets/ad/movie24.jpeg",
   },
   {
-    href: "https://www.aldoshoes.com/us/en_US?gclsrc=aw.ds&&utm_source=google&utm_medium=cpc&utm_campaign=aldo_us_en_search_brand_nogender_core&utm_content=653404197253&utm_term=aldo&device=c&network=g&placement=&gad_source=1&gclid=EAIaIQobChMI1_jp_KaxiQMVrm9HAR0_Sj1YEAAYASAAEgKjY_D_BwE",
+    href: mockHref,
     src: "/manzashops/assets/ad/domain.jpeg",
   },
   {
-    href: "https://www.aldoshoes.com/us/en_US?gclsrc=aw.ds&&utm_source=google&utm_medium=cpc&utm_campaign=aldo_us_en_search_brand_nogender_core&utm_content=653404197253&utm_term=aldo&device=c&network=g&placement=&gad_source=1&gclid=EAIaIQobChMI1_jp_KaxiQMVrm9HAR0_Sj1YEAAYASAAEgKjY_D_BwE",
+    href: mockHref,
     src: "/manzashops/assets/ad/movie31.jpeg",
   },
   {
-    href: "https://www.aldoshoes.com/us/en_US?gclsrc=aw.ds&&utm_source=google&utm_medium=cpc&utm_campaign=aldo_us_en_search_brand_nogender_core&utm_content=653404197253&utm_term=aldo&device=c&network=g&placement=&gad_source=1&gclid=EAIaIQobChMI1_jp_KaxiQMVrm9HAR0_Sj1YEAAYASAAEgKjY_D_BwE",
+    href: mockHref,
     src: "/manzashops/assets/ad/manza.png",
   },
   {
@@ -28,12 +28,13 @@ const data = [
     src: "/manzashops/assets/ad/webflow.jpeg",
   },
   {
-    href: "https://www.aldoshoes.com/us/en_US?gclsrc=aw.ds&&utm_source=google&utm_medium=cpc&utm_campaign=aldo_us_en_search_brand_nogender_core&utm_content=653404197253&utm_term=aldo&device=c&network=g&placement=&gad_source=1&gclid=EAIaIQobChMI1_jp_KaxiQMVrm9HAR0_Sj1YEAAYASAAEgKjY_D_BwE",
+    href: mockHref,
     src: "/manzashops/assets/ad/movie24.jpeg",
   },
 ];
 
-const mockHref = "";
+const mockHref =
+  "https://www.aldoshoes.com/us/en_US?gclsrc=aw.ds&&utm_source=google&utm_medium=cpc&utm_campaign=aldo_us_en_search_brand_nogender_core&utm_content=653404197253&utm_term=aldo&device=c&network=g&placement=&gad_source=1&gclid=EAIaIQobChMI1_jp_KaxiQMVrm9HAR0_Sj1YEAAYASAAEgKjY_D_BwE";
 
 const Content = () => {
   return (
