410ef868e6101d5e37dcc77d42f740aae416aa7a
bsegers <briansegs@gmail.com>
2024-11-25 22:51:58 -0500

    refactor: Extract variables

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,12 +1,14 @@
 import { navItems } from "./nav";
 import { langItems } from "./languages";
 import { mockHref } from "./content";
-import { block01BadgeGroup1Data } from "./badges/block_01_group1";
-import { block01BadgeGroup2Data } from "./badges/block_01_group2";
-import { block02BadgeGroup1Data } from "./badges/block_02_group1";
-import { block02BadgeGroup2Data } from "./badges/block_02_group2";
-import { block03BadgeGroup1Data } from "./badges/block_03_group1";
-import { block03BadgeGroup2Data } from "./badges/block_03_group2";
+import {
+  block01BadgeGroup1Data,
+  block01BadgeGroup2Data,
+  block02BadgeGroup1Data,
+  block02BadgeGroup2Data,
+  block03BadgeGroup1Data,
+  block03BadgeGroup2Data,
+} from "./badges/badgeBlockGroups";
 
 export {
   navItems,
