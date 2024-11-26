3a34f24c875897b2e3ac475742eec41735adfa31
bsegers <briansegs@gmail.com>
2024-11-25 19:53:31 -0500

    add: badge data to block 1 group 1

---
+++ src/constants/badges/bang-4-ur-buck/sections/section03/section03Categories.js
@@ -0,0 +1,33 @@
+import { group01 } from "./categoryProducts/group01";
+import { group02 } from "./categoryProducts/group02";
+import { group03 } from "./categoryProducts/group03";
+import { group04 } from "./categoryProducts/group04";
+import { group05 } from "./categoryProducts/group05";
+
+export const section03Categories = [
+  {
+    id: "cat0011",
+    categoryTitle: "Fitness Gear",
+    products: group01,
+  },
+  {
+    id: "cat0012",
+    categoryTitle: "Travel Must-Haves",
+    products: group02,
+  },
+  {
+    id: "cat0013",
+    categoryTitle: "Seasonal Trends",
+    products: group03,
+  },
+  {
+    id: "cat0014",
+    categoryTitle: "Luxury Picks",
+    products: group04,
+  },
+  {
+    id: "cat0015",
+    categoryTitle: "Tech Accessories",
+    products: group05,
+  },
+];
---
+++ src/constants/badges/bang-4-ur-buck/sections/section06/section06Categories.js
@@ -0,0 +1,33 @@
+import { group01 } from "./categoryProducts/group01";
+import { group02 } from "./categoryProducts/group02";
+import { group03 } from "./categoryProducts/group03";
+import { group04 } from "./categoryProducts/group04";
+import { group05 } from "./categoryProducts/group05";
+
+export const section06Categories = [
+  {
+    id: "cat0036",
+    categoryTitle: "Outdoor Exploration",
+    products: group01,
+  },
+  {
+    id: "cat0037",
+    categoryTitle: "Gadgets & Gizmos",
+    products: group02,
+  },
+  {
+    id: "cat0038",
+    categoryTitle: "Home Organization",
+    products: group03,
+  },
+  {
+    id: "cat0039",
+    categoryTitle: "Travel Essentials",
+    products: group04,
+  },
+  {
+    id: "cat0040",
+    categoryTitle: "Eco-Friendly Products",
+    products: group05,
+  },
+];
