6a4f3fc71df8795004611160d6734c6559bf1712
bsegers <briansegs@gmail.com>
2024-12-01 12:46:25 -0500

    fix: active styling

--- src/App.jsx
+++ src/App.jsx
@@ -14,17 +14,17 @@ import GreatThingsToTry from "./pages/GreatThingsToTry";
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}>
+      <Route path="/manzashops/" element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="*" element={<NoPage />} />
-        <Route path="/dash" element={<Dash />} />
-        <Route path="/deals" element={<Deals />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/jobs" element={<Jobs />} />
-        <Route path="/menus" element={<Menus />} />
-        <Route path="/movies" element={<Movies />} />
-        <Route path="/newsletter" element={<Newsletter />} />
-        <Route path="/great-things-to-try" element={<GreatThingsToTry />} />
+        <Route path="dash" element={<Dash />} />
+        <Route path="deals" element={<Deals />} />
+        <Route path="services" element={<Services />} />
+        <Route path="jobs" element={<Jobs />} />
+        <Route path="menus" element={<Menus />} />
+        <Route path="movies" element={<Movies />} />
+        <Route path="newsletter" element={<Newsletter />} />
+        <Route path="great-things-to-try" element={<GreatThingsToTry />} />
       </Route>
     </Routes>
   );
