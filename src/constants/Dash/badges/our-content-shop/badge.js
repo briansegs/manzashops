61ceeecfeb2c2bb669a87a96b4d2f0b6af6a732e
bsegers <briansegs@gmail.com>
2024-12-07 10:46:26 -0500

    feat: Add Ad data to quickview container

--- src/constants/Dash/badges/our-content-shop/badge.js
+++ src/constants/Dash/badges/our-content-shop/badge.js
@@ -55,5 +55,75 @@ export const ourContentShop = {
       sectionImg: "/manzashops/assets/badge/YouTube Thumbnail-2.png",
       categories: section07Categories,
     },
+    {
+      id: "adSec0001",
+      ad: true,
+      sectionName: "Ads 1",
+      ads: {
+        ad01: {
+          href: "/",
+          img: "/manzashops/assets/ad/clothes2.png",
+          alt: "ad 1",
+        },
+        ad02: {
+          href: "/",
+          img: "/manzashops/assets/ad/ads2.jpeg",
+          alt: "ad 2",
+        },
+        ad03: {
+          href: "/",
+          img: "/manzashops/assets/ad/ads3.jpeg",
+          alt: "ad 3",
+        },
+        ad04: {
+          href: "/",
+          img: "/manzashops/assets/ad/ads4.jpeg",
+          alt: "ad 4",
+        },
+        ad05: {
+          id: "qvadrow0001",
+          href: "/",
+          img: "/manzashops/assets/ad/styles.jpeg",
+          alt: "ad 5",
+        },
+        ad06: {
+          id: "qvadrow0002",
+          href: "/",
+          img: "/manzashops/assets/ad/collection1.jpeg",
+          alt: "ad 6",
+        },
+        ad07: {
+          id: "qvadrow0003",
+          href: "/",
+          img: "/manzashops/assets/ad/perfume2.jpeg",
+          alt: "ad 7",
+        },
+        ad08: {
+          href: "/",
+          img: "/manzashops/assets/ad/clothes.jpeg",
+          alt: "ad 8",
+        },
+        ad09: {
+          href: "/",
+          img: "/manzashops/assets/ad/xboxad.jpeg",
+          alt: "ad 8",
+        },
+        ad10: {
+          href: "/",
+          img: "/manzashops/assets/ad/playstationad1.jpeg",
+          alt: "ad 8",
+        },
+        ad11: {
+          href: "/",
+          img: "/manzashops/assets/ad/game1.jpeg",
+          alt: "ad 8",
+        },
+        ad12: {
+          href: "/",
+          img: "/manzashops/assets/ad/tech.jpeg",
+          alt: "ad 8",
+        },
+      },
+    },
   ],
 };
