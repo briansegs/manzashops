4720d5c7514c3d5a027bd227a135a9055d81dfd0
bsegers <briansegs@gmail.com>
2024-12-14 17:46:31 -0500

    feat: Add 3 more images to dash hero

--- src/components/Dash/ImageSlider.jsx
+++ src/components/Dash/ImageSlider.jsx
@@ -1,13 +1,19 @@
+import { sliderData } from "../../constants/Dash/hero";
+
 const ImageSlider = () => {
   return (
     <>
       <div className="relative flex-1">
-        <a href="">
-          <img
-            src="/manzashops/assets/manza.png"
-            className="w-[171vh] h-[59.5vh] object-fit rounded-[10px] border-secondary border-b-4 shadow-[10px_10px_10px_black]"
-          />
-        </a>
+        <div className="flex w-full overflow-x-auto">
+          {sliderData.map(({ id, href, src, styles }) => (
+            <a key={id} href={href} className="w-full flex-shrink-0">
+              <img
+                src={src}
+                className={`w-[171vh] h-[472.8px] object-fit rounded-[10px] shadow-[10px_10px_10px_black] ${styles}`}
+              />
+            </a>
+          ))}
+        </div>
 
         <div className="border-white border-[1px] w-fit h-[4vh] pl-1 pr-3 bg-black absolute text-white rounded-tr-[10px] rounded-tl-[10px] bottom-10 sm:right-96 right-20">
           <p>Hover & Scroll {">>>"}</p>
