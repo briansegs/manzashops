684c5ebcb70dad094f64862cddd3f9c957b5f471
bsegers <briansegs@gmail.com>
2024-12-14 17:04:45 -0500

    fix: Overflow

--- src/components/Dash/AdRow02.jsx
+++ src/components/Dash/AdRow02.jsx
@@ -2,7 +2,7 @@ const AdRow02 = ({ ad1Href, ad1Src, ad1Alt, ad2Href, ad2Src, ad2Alt }) => (
   <div className="bg-black rounded-[10px] xl:w-fit md:w-[95%] w-[99%]">
     <h3 className="text-white text-[24px] pl-5 pt-2">Ads</h3>
 
-    <div className="h-[440px] bg-black rounded-[10px] items-center px-10 pb-10 pt-3 flex gap-8 xl:justify-center  overflow-x-auto">
+    <div className="h-[440px] bg-black rounded-[10px] items-center px-10 pb-10 pt-3 flex gap-8 xl:justify-center overflow-x-auto">
       <div className="bg-slate-200 w-96 h-full rounded-[10px] shrink-0 border-[2px] border-white hover:border-secondary">
         <a href={ad1Href}>
           <img
--- src/components/Dash/AdRow04.jsx
+++ src/components/Dash/AdRow04.jsx
@@ -2,7 +2,7 @@ const AdRow04 = ({ ad1Href, ad1Src, ad1Alt, ad2Href, ad2Src, ad2Alt }) => (
   <div className="bg-black rounded-[10px] xl:w-fit md:w-[95%] w-[99%]">
     <h3 className="text-white text-[24px] pl-5 pt-2">Ads</h3>
 
-    <div className="h-[400px] bg-black xl:w-fit md:w-[95%] w-[99%] rounded-[10px] items-center px-10 pb-10 pt-3 flex gap-8 xl:justify-center  overflow-x-auto">
+    <div className="h-[440px]  items-center px-10 pb-10 pt-3 flex gap-8 xl:justify-center  overflow-x-auto">
       <div className="bg-slate-200 w-[600px] h-full rounded-[10px] shrink-0 border-[2px] border-white hover:border-secondary">
         <a href={ad1Href}>
           <img
