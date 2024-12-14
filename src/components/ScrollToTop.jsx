ebd1d00578a73493f31841e8e038b2a021a9aab6
bsegers <briansegs@gmail.com>
2024-12-14 16:00:19 -0500

    feat: Create return to top button

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -14,7 +14,7 @@ const ScrollToTop = () => {
       onClick={scrollToTop}
       className="capitalize text-[28px] text-white border-[1px] border-[#1b1e23] bg-black rounded-full flex size-[100px] justify-center items-center hover:text-secondary shadow-[3px_3px_3px_white] hover:shadow-[3px_3px_3px_#60b3d1] overflow-hidden flex-shrink-0"
     >
-      <RxPinTop className="size-12" />
+      <RxPinTop className="size-11" />
     </button>
   );
 };
