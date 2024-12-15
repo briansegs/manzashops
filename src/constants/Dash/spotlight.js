4171926a32a4748f78aa210996c084ffe0259c74
JosephGpro <josephG94@protonmail.com>
2024-12-14 22:35:21 -0500

    changes made 17

--- src/constants/Dash/spotlight.js
+++ src/constants/Dash/spotlight.js
@@ -9,13 +9,13 @@ export const itemData = [
     id: "spt002",
     href: "https://www.aldoshoes.com/us/en_US?gclsrc=aw.ds&&utm_source=google&utm_medium=cpc&utm_campaign=aldo_us_en_search_brand_nogender_core&utm_content=653404197253&utm_term=aldo&device=c&network=g&placement=&gad_source=1&gclid=EAIaIQobChMI1_jp_KaxiQMVrm9HAR0_Sj1YEAAYASAAEgKjY_D_BwE",
     src: "/manzashops/assets/aldo.jpeg",
-    title: "aldo",
+    title: "comp 1",
   },
   {
     id: "spt003",
     href: "https://www.aldoshoes.com/us/en_US?gclsrc=aw.ds&&utm_source=google&utm_medium=cpc&utm_campaign=aldo_us_en_search_brand_nogender_core&utm_content=653404197253&utm_term=aldo&device=c&network=g&placement=&gad_source=1&gclid=EAIaIQobChMI1_jp_KaxiQMVrm9HAR0_Sj1YEAAYASAAEgKjY_D_BwE",
     src: "/manzashops/assets/aldo.jpeg",
-    title: "aldo",
+    title: "comp 2",
   },
   {
     id: "spt004",
