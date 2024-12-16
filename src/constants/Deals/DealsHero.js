ce3f98613caf5da2625eac750d63214ac285ab51
bsegers <briansegs@gmail.com>
2024-12-16 13:23:15 -0500

    feat: Add Deals page

---
+++ src/constants/Deals/DealsHero.js
@@ -0,0 +1,26 @@
+export const sliderData = [
+  {
+    id: "img0001",
+    href: "",
+    src: "/manzashops/assets/cover1.png",
+    styles: "border-secondary border-b-4",
+  },
+  {
+    id: "img0002",
+    href: "",
+    src: "/manzashops/assets/cover1.png",
+    styles: "",
+  },
+  {
+    id: "img0003",
+    href: "",
+    src: "/manzashops/assets/cover1.png",
+    styles: "",
+  },
+  {
+    id: "img0004",
+    href: "",
+    src: "/manzashops/assets/cover1.png",
+    styles: "",
+  },
+];
