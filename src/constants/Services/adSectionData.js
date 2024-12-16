c40c3b08f877b87d3979df8fd6f52482c6c621d0
bsegers <briansegs@gmail.com>
2024-12-16 13:40:57 -0500

    feat: Add Services page

---
+++ src/constants/Services/adSectionData.js
@@ -0,0 +1,497 @@
+export const adSectionData01 = {
+  id: "gtttsec0001",
+  btnImg: "",
+  title: "top",
+  subTitle: "games to try",
+  adRow1: [
+    {
+      id: "adrow10001",
+      href: "",
+      src: "/manzashops/assets/ad/webflow.jpeg",
+      alt: "ad1",
+    },
+    {
+      id: "adrow10002",
+      href: "",
+      src: "/manzashops/assets/ad/domain.jpeg",
+      alt: "ad2",
+    },
+    {
+      id: "adrow10003",
+      href: "",
+      src: "/manzashops/assets/ad/movie24.jpeg",
+      alt: "ad3",
+    },
+    {
+      id: "adrow10004",
+      href: "",
+      src: "/manzashops/assets/ad/movie31.jpeg",
+      alt: "ad4",
+    },
+    {
+      id: "adrow10005",
+      href: "",
+      src: "/manzashops/assets/english.jpeg",
+      alt: "ad5",
+    },
+    {
+      id: "adrow10006",
+      href: "",
+      src: "/manzashops/assets/max.jpeg",
+      alt: "ad6",
+    },
+  ],
+  adRow2: [
+    {
+      id: "adrow20001",
+      href: "",
+      src: "/manzashops/assets/ad/movie31.jpeg",
+      alt: "ad1",
+    },
+    {
+      id: "adrow20002",
+      href: "",
+      src: "/manzashops/assets/english.jpeg",
+      alt: "ad2",
+    },
+    {
+      id: "adrow20003",
+      href: "",
+      src: "/manzashops/assets/max.jpeg",
+      alt: "ad3",
+    },
+    {
+      id: "adrow20004",
+      href: "",
+      src: "/manzashops/assets/ad/webflow.jpeg",
+      alt: "ad4",
+    },
+    {
+      id: "adrow20005",
+      href: "",
+      src: "/manzashops/assets/ad/domain.jpeg",
+      alt: "ad5",
+    },
+    {
+      id: "adrow20006",
+      href: "",
+      src: "/manzashops/assets/ad/movie24.jpeg",
+      alt: "ad6",
+    },
+  ],
+};
+
+export const adSectionData02 = {
+  id: "gtttsec0002",
+  btnImg: "",
+  title: "hit",
+  subTitle: "games to try",
+  adRow1: [
+    {
+      id: "adrow10007",
+      href: "",
+      src: "/manzashops/assets/ad/webflow.jpeg",
+      alt: "ad1",
+    },
+    {
+      id: "adrow10008",
+      href: "",
+      src: "/manzashops/assets/ad/domain.jpeg",
+      alt: "ad2",
+    },
+    {
+      id: "adrow10009",
+      href: "",
+      src: "/manzashops/assets/ad/movie24.jpeg",
+      alt: "ad3",
+    },
+    {
+      id: "adrow10010",
+      href: "",
+      src: "/manzashops/assets/ad/movie31.jpeg",
+      alt: "ad4",
+    },
+    {
+      id: "adrow10011",
+      href: "",
+      src: "/manzashops/assets/english.jpeg",
+      alt: "ad5",
+    },
+    {
+      id: "adrow10012",
+      href: "",
+      src: "/manzashops/assets/max.jpeg",
+      alt: "ad6",
+    },
+  ],
+  adRow2: [
+    {
+      id: "adrow20007",
+      href: "",
+      src: "/manzashops/assets/ad/movie31.jpeg",
+      alt: "ad1",
+    },
+    {
+      id: "adrow20008",
+      href: "",
+      src: "/manzashops/assets/english.jpeg",
+      alt: "ad2",
+    },
+    {
+      id: "adrow20009",
+      href: "",
+      src: "/manzashops/assets/max.jpeg",
+      alt: "ad3",
+    },
+    {
+      id: "adrow20010",
+      href: "",
+      src: "/manzashops/assets/ad/webflow.jpeg",
+      alt: "ad4",
+    },
+    {
+      id: "adrow20011",
+      href: "",
+      src: "/manzashops/assets/ad/domain.jpeg",
+      alt: "ad5",
+    },
+    {
+      id: "adrow20012",
+      href: "",
+      src: "/manzashops/assets/ad/movie24.jpeg",
+      alt: "ad6",
+    },
+  ],
+};
+
+export const adSectionData03 = {
+  id: "gtttsec0003",
+  btnImg: "",
+  title: "piano",
+  subTitle: "games to try",
+  adRow1: [
+    {
+      id: "adrow10013",
+      href: "",
+      src: "/manzashops/assets/ad/webflow.jpeg",
+      alt: "ad1",
+    },
+    {
+      id: "adrow10014",
+      href: "",
+      src: "/manzashops/assets/ad/domain.jpeg",
+      alt: "ad2",
+    },
+    {
+      id: "adrow10015",
+      href: "",
+      src: "/manzashops/assets/ad/movie24.jpeg",
+      alt: "ad3",
+    },
+    {
+      id: "adrow10016",
+      href: "",
+      src: "/manzashops/assets/ad/movie31.jpeg",
+      alt: "ad4",
+    },
+    {
+      id: "adrow10017",
+      href: "",
+      src: "/manzashops/assets/english.jpeg",
+      alt: "ad5",
+    },
+    {
+      id: "adrow10018",
+      href: "",
+      src: "/manzashops/assets/max.jpeg",
+      alt: "ad6",
+    },
+  ],
+  adRow2: [
+    {
+      id: "adrow20013",
+      href: "",
+      src: "/manzashops/assets/ad/movie31.jpeg",
+      alt: "ad1",
+    },
+    {
+      id: "adrow20014",
+      href: "",
+      src: "/manzashops/assets/english.jpeg",
+      alt: "ad2",
+    },
+    {
+      id: "adrow20015",
+      href: "",
+      src: "/manzashops/assets/max.jpeg",
+      alt: "ad3",
+    },
+    {
+      id: "adrow20016",
+      href: "",
+      src: "/manzashops/assets/ad/webflow.jpeg",
+      alt: "ad4",
+    },
+    {
+      id: "adrow20017",
+      href: "",
+      src: "/manzashops/assets/ad/domain.jpeg",
+      alt: "ad5",
+    },
+    {
+      id: "adrow20018",
+      href: "",
+      src: "/manzashops/assets/ad/movie24.jpeg",
+      alt: "ad6",
+    },
+  ],
+};
+
+export const adSectionData04 = {
+  id: "gtttsec0004",
+  btnImg: "",
+  title: "surf",
+  subTitle: "games to try",
+  adRow1: [
+    {
+      id: "adrow10019",
+      href: "",
+      src: "/manzashops/assets/ad/webflow.jpeg",
+      alt: "ad1",
+    },
+    {
+      id: "adrow10020",
+      href: "",
+      src: "/manzashops/assets/ad/domain.jpeg",
+      alt: "ad2",
+    },
+    {
+      id: "adrow10021",
+      href: "",
+      src: "/manzashops/assets/ad/movie24.jpeg",
+      alt: "ad3",
+    },
+    {
+      id: "adrow10022",
+      href: "",
+      src: "/manzashops/assets/ad/movie31.jpeg",
+      alt: "ad4",
+    },
+    {
+      id: "adrow10023",
+      href: "",
+      src: "/manzashops/assets/english.jpeg",
+      alt: "ad5",
+    },
+    {
+      id: "adrow10024",
+      href: "",
+      src: "/manzashops/assets/max.jpeg",
+      alt: "ad6",
+    },
+  ],
+  adRow2: [
+    {
+      id: "adrow20019",
+      href: "",
+      src: "/manzashops/assets/ad/movie31.jpeg",
+      alt: "ad1",
+    },
+    {
+      id: "adrow20020",
+      href: "",
+      src: "/manzashops/assets/english.jpeg",
+      alt: "ad2",
+    },
+    {
+      id: "adrow20021",
+      href: "",
+      src: "/manzashops/assets/max.jpeg",
+      alt: "ad3",
+    },
+    {
+      id: "adrow20022",
+      href: "",
+      src: "/manzashops/assets/ad/webflow.jpeg",
+      alt: "ad4",
+    },
+    {
+      id: "adrow20023",
+      href: "",
+      src: "/manzashops/assets/ad/domain.jpeg",
+      alt: "ad5",
+    },
+    {
+      id: "adrow20024",
+      href: "",
+      src: "/manzashops/assets/ad/movie24.jpeg",
+      alt: "ad6",
+    },
+  ],
+};
+
+export const adSectionData05 = {
+  id: "gtttsec0005",
+  btnImg: "",
+  title: "tech",
+  subTitle: "games to try",
+  adRow1: [
+    {
+      id: "adrow10025",
+      href: "",
+      src: "/manzashops/assets/ad/webflow.jpeg",
+      alt: "ad1",
+    },
+    {
+      id: "adrow10026",
+      href: "",
+      src: "/manzashops/assets/ad/domain.jpeg",
+      alt: "ad2",
+    },
+    {
+      id: "adrow10027",
+      href: "",
+      src: "/manzashops/assets/ad/movie24.jpeg",
+      alt: "ad3",
+    },
+    {
+      id: "adrow10028",
+      href: "",
+      src: "/manzashops/assets/ad/movie31.jpeg",
+      alt: "ad4",
+    },
+    {
+      id: "adrow10029",
+      href: "",
+      src: "/manzashops/assets/english.jpeg",
+      alt: "ad5",
+    },
+    {
+      id: "adrow10030",
+      href: "",
+      src: "/manzashops/assets/max.jpeg",
+      alt: "ad6",
+    },
+  ],
+  adRow2: [
+    {
+      id: "adrow20025",
+      href: "",
+      src: "/manzashops/assets/ad/movie31.jpeg",
+      alt: "ad1",
+    },
+    {
+      id: "adrow20026",
+      href: "",
+      src: "/manzashops/assets/english.jpeg",
+      alt: "ad2",
+    },
+    {
+      id: "adrow20027",
+      href: "",
+      src: "/manzashops/assets/max.jpeg",
+      alt: "ad3",
+    },
+    {
+      id: "adrow20028",
+      href: "",
+      src: "/manzashops/assets/ad/webflow.jpeg",
+      alt: "ad4",
+    },
+    {
+      id: "adrow20029",
+      href: "",
+      src: "/manzashops/assets/ad/domain.jpeg",
+      alt: "ad5",
+    },
+    {
+      id: "adrow20030",
+      href: "",
+      src: "/manzashops/assets/ad/movie24.jpeg",
+      alt: "ad6",
+    },
+  ],
+};
+
+export const adSectionData06 = {
+  id: "gtttsec0006",
+  btnImg: "",
+  title: "dress",
+  subTitle: "games to try",
+  adRow1: [
+    {
+      id: "adrow10031",
+      href: "",
+      src: "/manzashops/assets/ad/webflow.jpeg",
+      alt: "ad1",
+    },
+    {
+      id: "adrow10032",
+      href: "",
+      src: "/manzashops/assets/ad/domain.jpeg",
+      alt: "ad2",
+    },
+    {
+      id: "adrow10033",
+      href: "",
+      src: "/manzashops/assets/ad/movie24.jpeg",
+      alt: "ad3",
+    },
+    {
+      id: "adrow10034",
+      href: "",
+      src: "/manzashops/assets/ad/movie31.jpeg",
+      alt: "ad4",
+    },
+    {
+      id: "adrow10035",
+      href: "",
+      src: "/manzashops/assets/english.jpeg",
+      alt: "ad5",
+    },
+    {
+      id: "adrow10036",
+      href: "",
+      src: "/manzashops/assets/max.jpeg",
+      alt: "ad6",
+    },
+  ],
+  adRow2: [
+    {
+      id: "adrow20031",
+      href: "",
+      src: "/manzashops/assets/ad/movie31.jpeg",
+      alt: "ad1",
+    },
+    {
+      id: "adrow20032",
+      href: "",
+      src: "/manzashops/assets/english.jpeg",
+      alt: "ad2",
+    },
+    {
+      id: "adrow20033",
+      href: "",
+      src: "/manzashops/assets/max.jpeg",
+      alt: "ad3",
+    },
+    {
+      id: "adrow20034",
+      href: "",
+      src: "/manzashops/assets/ad/webflow.jpeg",
+      alt: "ad4",
+    },
+    {
+      id: "adrow20035",
+      href: "",
+      src: "/manzashops/assets/ad/domain.jpeg",
+      alt: "ad5",
+    },
+    {
+      id: "adrow20036",
+      href: "",
+      src: "/manzashops/assets/ad/movie24.jpeg",
+      alt: "ad6",
+    },
+  ],
+};
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,7 +1,35 @@
+import ServicesHero from "../components/Services/ServicesHero";
+import ServicesSection from "../components/Services/ServicesSection";
+import ServicesSuggestions from "../components/Services/ServicesSuggestions";
+import {
+  adSectionData01,
+  adSectionData02,
+  adSectionData03,
+  adSectionData04,
+  adSectionData05,
+  adSectionData06,
+} from "../constants/Deals";
+
 const Services = () => {
   return (
-    <section className="h-[50vh] flex justify-center items-center">
-      Services
+    <section className="bg-white w-full">
+      <ServicesHero />
+
+      <ServicesSuggestions />
+
+      <div className="flex flex-col items-center gap-10 w-full md:px-12 px-0 pt-2 pb-24 bg-[#121212]">
+        <ServicesSection data={adSectionData01} />
+
+        <ServicesSection data={adSectionData02} />
+
+        <ServicesSection data={adSectionData03} />
+
+        <ServicesSection data={adSectionData04} />
+
+        <ServicesSection data={adSectionData05} />
+
+        <ServicesSection data={adSectionData06} />
+      </div>
     </section>
   );
 };
