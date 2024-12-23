5a3bb66c6206c3affae17ed9b5f23bea3a297691
bsegers <briansegs@gmail.com>
2024-12-23 14:54:09 -0500

    feat: Increase quickview ad card height

--- src/components/Dash/AdCard.jsx
+++ src/components/Dash/AdCard.jsx
@@ -1,6 +1,6 @@
 const AdCard = ({ href, img, alt }) => {
   return (
-    <div className="border-[2px] border-white hover:border-secondary w-[419px] h-72 shrink-0">
+    <div className="border-[2px] border-white hover:border-secondary w-[419px] h-80 shrink-0">
       <a href={href}>
         <img src={img} alt={alt} className="object-fill size-full" />
       </a>
