3720e45a7ad3bcb19c7181fc46ffbb76dc5457f6
bsegers <briansegs@gmail.com>
2025-01-13 12:59:07 -0500

    feat: Change suggestion bar to match style and function of Dash

--- src/components/Jobs/JobsSuggestions.jsx
+++ src/components/Jobs/JobsSuggestions.jsx
@@ -1,49 +1,39 @@
+import { useState, useRef } from "react";
 import ScrollToTop from "../ScrollToTop";
-import {
-  adSectionData01,
-  adSectionData02,
-  adSectionData03,
-  adSectionData04,
-  adSectionData05,
-  adSectionData06,
-} from "../../constants/Jobs";
+import { suggetionsItems } from "../../constants/Jobs";
+import UseIntersectionObserver from "../../hooks/UseIntersectionObserver";
+import SuggestionsButton from "../SuggestionsButton";
 
 const JobsSuggestions = () => {
-  const scrollToElement = (id) => {
-    const targetElement = document.getElementById(id);
+  const [activeButton, setActiveButton] = useState(null);
+  const clickedButtonRef = useRef(null); // Track the last clicked button
+  const isScrollingRef = useRef(false); // Prevent updates during scrolling
 
-    targetElement?.scrollIntoView({
-      behavior: "smooth",
-      block: "start",
-    });
-  };
+  UseIntersectionObserver({
+    suggetionsItems,
+    activeButton,
+    setActiveButton,
+    clickedButtonRef,
+    isScrollingRef,
+  });
 
   return (
-    <div className="flex items-center gap-4 justify-between bg-[#141414] border-[3px] border-black flex-nowrap sticky px-2 top-0 z-50 overflow-x-auto h-32">
+    <div className="flex items-center gap-4 justify-between bg-[#141414] border-[3px] border-black flex-nowrap sticky px-3 top-0 z-50 overflow-x-auto h-24">
       <ScrollToTop />
 
-      {[
-        adSectionData01,
-        adSectionData02,
-        adSectionData03,
-        adSectionData04,
-        adSectionData05,
-        adSectionData06,
-        { id: "gtttsec0007", title: "glasses" },
-        { id: "gtttsec0008", title: "shoes" },
-        { id: "gtttsec0009", title: "jewelery" },
-        { id: "gtttsec0010", title: "carparts" },
-        { id: "gtttsec0011", title: "sports" },
-        { id: "gtttsec0012", title: "luxury" },
-      ].map(({ id, title }) => (
-        <button
+      {suggetionsItems.map(({ id, title }) => (
+        <SuggestionsButton
           key={id}
-          type="button"
-          onClick={() => scrollToElement(id)}
-          className="capitalize text-[28px] text-white border-[1px] border-[#1b1e23] bg-black rounded-full flex size-[100px] justify-center items-center hover:text-secondary shadow-[3px_3px_3px_white] hover:shadow-[3px_3px_3px_#60b3d1] overflow-hidden flex-shrink-0"
-        >
-          {title}
-        </button>
+          id={id}
+          title={title}
+          scrollingProps={{
+            activeButton,
+            isScrollingRef,
+            clickedButtonRef,
+            setActiveButton,
+          }}
+          offset={100}
+        />
       ))}
     </div>
   );
